ef7232c8424c02f83a9b40c54ba1479a000fb6fa
malachyfernandez <jag_pyloric.0j@icloud.com>
2026-03-18 19:05:22 -0400

    animation to row

--- app/components/game/UserRow.tsx
+++ app/components/game/UserRow.tsx
@@ -4,6 +4,7 @@ import InlineEditableText from '../ui/forms/InlineEditableText';
 import Column from '../layout/Column';
 import Row from '../layout/Row';
 import CustomCheckbox from '../ui/CustomCheckbox';
+import Animated, { FadeInLeft, FadeInRight, FadeOutDown, FadeOutLeft, FadeOutRight, Easing, FadeInDown, FadeOutUp } from 'react-native-reanimated';
 
 interface UserRowProps {
     user: {
@@ -61,35 +62,47 @@ const UserRow = ({ user, index, isLast, setLivingState, setExtraColumnValue, use
                 <CustomCheckbox checked={isDead} onChange={toggleLivingState} />
             </Column>
             <Column gap={0} className='w-28 h-full border border-subtle-border items-center justify-center'>
-                <InlineEditableText 
+                {/* <InlineEditableText
                     value={user.realName || ''}
-                    onChange={(newValue) => {/* Add handler for real name if needed */}}
+                    onChange={(newValue) => }}
                     placeholder='Unnamed player'
-                    className='mb-[-4px] w-20 text-center text-nowrap overflow-hidden' 
-                    weight='medium' 
-                />
+                    className='mb-[-4px] w-20 text-center text-nowrap overflow-hidden'
+                    weight='medium'
+                /> */}
+                <PoppinsText weight='medium'>{user.realName || 'No Name'}</PoppinsText>
                 <PoppinsText varient='subtext'>{user.role || 'No role'}</PoppinsText>
             </Column>
-        
+
             {user.playerData.extraColumns?.map((column, columnIndex) => {
                 if (!userTableColumnVisibility?.extraUserColumns[columnIndex]) return null;
-                
+
                 const visibleColumns = user.playerData.extraColumns?.filter((_, idx) => userTableColumnVisibility?.extraUserColumns[idx]) || [];
                 const visibleIndex = visibleColumns.indexOf(column);
                 const isLastVisibleColumn = visibleIndex === visibleColumns.length - 1;
-                
+
                 return (
-                    <Column key={columnIndex} className={`w-28 h-full border border-subtle-border items-center justify-center ${isLast && isLastVisibleColumn ? 'rounded-br-lg' : ''} ${editingColumns[columnIndex] ? 'z-50' : ''}`}>
-                        <InlineEditableText 
-                            value={column}
-                            onChange={(newValue) => setExtraColumnValue?.(index, columnIndex, newValue)}
-                            placeholder='UNSET'
-                            className='w-20 text-center text-nowrap overflow-hidden' 
-                            weight='medium' 
-                            onEditStart={() => handleColumnEditStart(columnIndex)}
-                            onEditEnd={() => handleColumnEditEnd(columnIndex)}
-                        />
-                    </Column>
+                    <Animated.View
+                        className={`${editingColumns[columnIndex] ? 'z-50' : ''}`}
+                        key={columnIndex}
+                        entering={
+                            FadeInDown.duration(100).easing(Easing.ease)
+                        }
+                        exiting={
+                            FadeOutUp.duration(100).easing(Easing.ease)
+                        }
+                    >
+                        <Column className={`w-28 h-full border border-subtle-border items-center justify-center ${isLast && isLastVisibleColumn ? 'rounded-br-lg' : ''} `}>
+                            <InlineEditableText
+                                value={column}
+                                onChange={(newValue) => setExtraColumnValue?.(index, columnIndex, newValue)}
+                                placeholder='UNSET'
+                                className='w-20 text-center text-nowrap overflow-hidden'
+                                weight='medium'
+                                onEditStart={() => handleColumnEditStart(columnIndex)}
+                                onEditEnd={() => handleColumnEditEnd(columnIndex)}
+                            />
+                        </Column>
+                    </Animated.View>
                 );
             })}
         </Row>
